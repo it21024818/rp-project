73fc2607f60c605419ecd6e662717be96e8bc8e9
Disira Thihan <it21070358@my.sliit.lk>
2024-05-07 15:26:48 +0530

    Updates on web extention.

--- web_ext/src/App.tsx
+++ web_ext/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import AuthLogin from "./components/Login/AuthLogin";
+import "./App.css";
+import * as React from "react";
+import { PaletteMode } from "@mui/material";
+import getLPTheme from "./themes/theme";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode] = React.useState<PaletteMode>("light");
+  const LPtheme = createTheme(getLPTheme(mode));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={LPtheme}>
+      {/* Add a button to toggle color mode */}
+      <AuthLogin />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- web_ext/vite.config.ts
+++ web_ext/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
+import manifest from "./public/manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), crx({ manifest })],
-	server: {
-		port: 3001,
-	},
+  plugins: [react(), crx({ manifest })],
+  server: {
+    port: 3001,
+  },
 });
