02e1d6b9fbc8cd29023469edf5b6257a55e5e0b2
Tharindu Gunasekera <gunasekeratharindu@gmail.com>
2024-08-27 18:31:43 +0530

    Fixed error in PaymentsModule instantiation

--- main_api/src/payments/payments.module.ts
+++ main_api/src/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { Plan, PlanSchema } from './plan.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Plan.name, schema: PlanSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Plan.name, schema: PlanSchema }]), UsersModule],
   providers: [PaymentsService],
   controllers: [PaymentsController],
   exports: [MongooseModule],
