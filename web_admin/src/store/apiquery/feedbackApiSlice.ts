b225d04704e2c9cf30d7e1cb427dbe433a9caa2d
Disira Thihan <87310369+it21070358@users.noreply.github.com>
2024-09-08 03:16:03 +0530

    updated api slicecs

--- web_admin/src/store/apiquery/feedbackApiSlice.ts
+++ web_admin/src/store/apiquery/feedbackApiSlice.ts
@@ -5,8 +5,8 @@ import { BASE_URL } from '../../Utils/Generals';
 const token =
   'yJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MjU3NDUyNTcsInN1YiI6IjY2Y2I3MGM1OWY2YTA2MjIxMmNkZjYxNiIsImlzcyI6IlVFRSIsImF1ZCI6WyJsaWdodC1ob3VzZS13ZWItYXBwIiwibGlnaHQtaG91c2UtbW9iaWxlLWFwcCIsImxpZ2h0LWhvdXNlLWFkbWluLWFwcCIsImxpZ2h0LWhvdXNlLWV4dGVuc2lvbiJdLCJleHAiOjE3MjU3NTI0NTd9.FUMHNXCdPQjNikQx9HEOBhIE2VTuzAgWqVIe5GCJmvFJWyXat38d7v7Zmf_3rlBilNCYVDjF4iZ1jAbTwFZpvSAMUgUyzwMA-ecjmuHk9ELQcdRzDT-jsCwMTh2PGf5VuQATvEqSSNNa5Y0Q0X3jXYUMZETJB9YsF76HSROE1rsqkbL7Wd9f4Gnbs3-v37nxwOoSeVkoRxWgmoaU-DYzBfpDhhZ2BTpA19e481sZgT8wdwYDRbBIIuITRcF1Yf2qKCW2aT67ue5F1XEPlnhAR7F5EHbW_r3uKWJH3zmKxcS-fCtZsNGgBLE9AmZ7WHZwRqqe02OrEn4DHe28mwfP0w';
 
-export const predictionApiSlice = createApi({
-  reducerPath: 'api/prediction',
+export const feedbackApiSlice = createApi({
+  reducerPath: 'api/feedback',
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
     prepareHeaders(headers) {
@@ -16,34 +16,34 @@ export const predictionApiSlice = createApi({
       return headers;
     }
   }),
-  tagTypes: ['Prediction'],
+  tagTypes: ['feedback'],
 
   endpoints: (builder) => ({
-    prediction: builder.mutation({
+    feedback: builder.mutation({
       query: (formData) => ({
-        url: `/v1/predictions`,
+        url: `/v1/feedbacks`,
         method: 'POST',
         body: formData
       }),
-      invalidatesTags: ['Prediction']
+      invalidatesTags: ['feedback']
     }),
-    getPredictions: builder.mutation({
+    getfeedbacks: builder.mutation({
       query: (formData) => ({
-        url: `/v1/predictions/search`,
+        url: `/v1/feedback/search`,
         method: 'POST',
         body: formData
       }),
-      invalidatesTags: ['Prediction']
+      invalidatesTags: ['feedback']
     }),
-    getPrediction: builder.query({
-      query: (id: string) => `/v1/predictions/${id}`,
-      providesTags: ['Prediction']
+    getfeedback: builder.query({
+      query: (id: string) => `/v1/feedback/${id}`,
+      providesTags: ['feedback']
     })
   })
 });
 
 export const {
-  usePredictionMutation,
-  useGetPredictionsMutation,
-  useGetPredictionQuery
-} = predictionApiSlice;
+  useFeedbackMutation,
+  useGetfeedbacksMutation,
+  useGetfeedbackQuery
+} = feedbackApiSlice;
