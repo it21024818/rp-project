efafd3329b25694665e89f711c250a38b04979f7
Disira Thihan <thihan@lakmobile.com>
2024-10-25 19:16:07 +0530

    Latest changers on web app

--- web_app/src/components/Reviews/Reviews.tsx
+++ web_app/src/components/Reviews/Reviews.tsx
@@ -1,264 +1,3 @@
-// import {
-//   Container,
-//   Typography,
-//   Button,
-//   Modal,
-//   TextField,
-//   Box,
-//   Stack,
-//   IconButton,
-//   Alert,
-//   MenuItem,
-//   Select,
-//   InputLabel,
-//   FormControl,
-//   Grid,
-//   Snackbar,
-// } from "@mui/material";
-// import CloseIcon from "@mui/icons-material/Close";
-// import { useState } from "react";
-// import { styled } from "@mui/material/styles";
-// import Rating from "@mui/material/Rating";
-// import { SelectChangeEvent } from "@mui/material";
-// import { useReviewsMutation } from "../../store/apiquery/reviewsApiSlice";
-// import ThumbUpIcon from "@mui/icons-material/ThumbUp";
-// import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
-
-// const SentimentEnum = {
-//   NEGATIVE: "NEGATIVE",
-//   POSITIVE: "POSITIVE",
-//   NEUTRAL: "NEUTRAL",
-// };
-
-// const SarcasmEnum = {
-//   GENERIC: "GENERIC",
-//   RHETORICAL_QUESTION: "RHETORICAL_QUESTION",
-//   HYPERBOLE: "HYPERBOLE",
-// };
-
-// const PoliticalLeaningEnum = {
-//   LEFT: "LEFT",
-//   RIGHT: "RIGHT",
-//   CENTER: "CENTER",
-// };
-
-// const ReactionEnum = {
-//   GOOD: "GOOD",
-//   BAD: "BAD",
-// };
-
-// const StyledRating = styled(Rating)({
-//   "& .MuiRating-iconFilled": {
-//     color: "#ff6d75",
-//   },
-//   "& .MuiRating-iconHover": {
-//     color: "#ff3d47",
-//   },
-// });
-
-// interface ReviewsProps {
-//   id: string;
-// }
-
-// export default function Reviews({ id }: ReviewsProps) {
-//   const [makeFeedback] = useReviewsMutation();
-//   const [open, setOpen] = useState(false);
-//   const [newReview, setNewReview] = useState({
-//     reaction: "",
-//     details: {
-//       message: "",
-//       sentiment: "",
-//       sarcasm: "",
-//       bias: "",
-//       isFake: false,
-//     },
-//   });
-//   const [alertOpen, setAlertOpen] = useState(false);
-//   const [alertType, setAlertType] = useState<"success" | "error">("success");
-
-//   const handleOpen = () => setOpen(true);
-//   const handleClose = () => setOpen(false);
-
-//   const handleInputChange = (
-//     event: React.ChangeEvent<HTMLInputElement> | SelectChangeEvent<string>
-//   ) => {
-//     const { name, value } = event.target;
-
-//     setNewReview((prevReview) => ({
-//       ...prevReview,
-//       details: {
-//         ...prevReview.details,
-//         [name]: value,
-//       },
-//     }));
-//   };
-
-//   const handleSubmitReview = async () => {
-//     try {
-//       const predictionId = id;
-//       await makeFeedback({ predictionId, formData: newReview }).unwrap();
-//       setAlertType("success");
-//       setAlertOpen(true);
-//       handleClose();
-//     } catch (err) {
-//       setAlertType("error");
-//       setAlertOpen(true);
-//       console.error("Error submitting review:", err);
-//     }
-//   };
-
-//   const handleTextFieldChange = (
-//     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-//   ) => {
-//     const { name, value } = event.target;
-//     setNewReview((prevReview) => ({
-//       ...prevReview,
-//       details: {
-//         ...prevReview.details,
-//         [name]: value,
-//       },
-//     }));
-//   };
-
-//   const handleAlertClose = () => {
-//     setAlertOpen(false);
-//   };
-
-//   return (
-//     <Container sx={{ marginTop: "20px", marginBottom: "20px" }}>
-//       <Typography variant="h5">Add a review</Typography>
-
-//       <Button
-//         variant="contained"
-//         sx={{ marginTop: "20px" }}
-//         color="primary"
-//         onClick={handleOpen}
-//       >
-//         Add Review
-//       </Button>
-
-//       <Modal open={open} onClose={handleClose}>
-//         <Box
-//           sx={{
-//             position: "absolute",
-//             top: "50%",
-//             left: "50%",
-//             transform: "translate(-50%, -50%)",
-//             width: 400,
-//             bgcolor: "background.paper",
-//             p: 4,
-//           }}
-//         >
-//           <Stack
-//             direction="row"
-//             justifyContent="space-between"
-//             alignItems="center"
-//           >
-//             <Typography variant="h6">Add Your Review</Typography>
-//             <IconButton onClick={handleClose}>
-//               <CloseIcon />
-//             </IconButton>
-//           </Stack>
-//           <Grid container spacing={2}>
-//             <Grid item xs={8}>
-//               <Typography sx={{ color: "red", marginTop: "20px" }}>
-//                 Good Or Bad
-//               </Typography>
-//             </Grid>
-//             <Grid item xs={4}>
-//               <StyledRating
-//                 sx={{ marginTop: "20px" }}
-//                 name="reaction"
-//                 value={newReview.reaction === ReactionEnum.GOOD ? 1 : 0}
-//                 onChange={(_event, newValue) => {
-//                   setNewReview({
-//                     ...newReview,
-//                     reaction:
-//                       newValue === 1 ? ReactionEnum.GOOD : ReactionEnum.BAD,
-//                   });
-//                 }}
-//                 precision={1}
-//                 max={1}
-//                 icon={<ThumbUpIcon />}
-//                 emptyIcon={<ThumbUpOutlinedIcon />}
-//               />
-//             </Grid>
-//           </Grid>
-
-//           <TextField
-//             fullWidth
-//             margin="normal"
-//             label="Message"
-//             name="message"
-//             variant="outlined"
-//             value={newReview.details.message}
-//             onChange={handleTextFieldChange}
-//           />
-//           <FormControl fullWidth margin="normal">
-//             <InputLabel>Sentiment</InputLabel>
-//             <Select
-//               name="sentiment"
-//               value={newReview.details.sentiment}
-//               onChange={handleInputChange}
-//             >
-//               <MenuItem value={SentimentEnum.NEGATIVE}>Negative</MenuItem>
-//               <MenuItem value={SentimentEnum.POSITIVE}>Positive</MenuItem>
-//               <MenuItem value={SentimentEnum.NEUTRAL}>Neutral</MenuItem>
-//             </Select>
-//           </FormControl>
-//           <FormControl fullWidth margin="normal">
-//             <InputLabel>Sarcasm</InputLabel>
-//             <Select
-//               name="sarcasm"
-//               value={newReview.details.sarcasm}
-//               onChange={handleInputChange}
-//             >
-//               <MenuItem value={SarcasmEnum.GENERIC}>Generic</MenuItem>
-//               <MenuItem value={SarcasmEnum.RHETORICAL_QUESTION}>
-//                 Rhetorical Question
-//               </MenuItem>
-//               <MenuItem value={SarcasmEnum.HYPERBOLE}>Hyperbole</MenuItem>
-//             </Select>
-//           </FormControl>
-//           <FormControl fullWidth margin="normal">
-//             <InputLabel>Bias</InputLabel>
-//             <Select
-//               name="bias"
-//               value={newReview.details.bias}
-//               onChange={handleInputChange}
-//             >
-//               <MenuItem value={PoliticalLeaningEnum.LEFT}>Left</MenuItem>
-//               <MenuItem value={PoliticalLeaningEnum.CENTER}>Center</MenuItem>
-//               <MenuItem value={PoliticalLeaningEnum.RIGHT}>Right</MenuItem>
-//             </Select>
-//           </FormControl>
-//           <Button
-//             variant="contained"
-//             color="primary"
-//             onClick={handleSubmitReview}
-//             sx={{ mt: 2 }}
-//           >
-//             Submit Review
-//           </Button>
-//         </Box>
-//       </Modal>
-
-//       {/* Snackbar for success/error alerts */}
-//       <Snackbar
-//         open={alertOpen}
-//         onClose={handleAlertClose}
-//         autoHideDuration={5000}
-//         anchorOrigin={{ vertical: "top", horizontal: "right" }}
-//       >
-//         <Alert severity={alertType} onClose={handleAlertClose}>
-//           {alertType === "success"
-//             ? "Review added successfully!"
-//             : "Failed to add review."}
-//         </Alert>
-//       </Snackbar>
-//     </Container>
-//   );
-// }
 import {
   Container,
   Typography,
@@ -284,6 +23,8 @@ import { SelectChangeEvent } from "@mui/material";
 import { useReviewsMutation } from "../../store/apiquery/reviewsApiSlice";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import { ThumbDownOutlined } from "@mui/icons-material";
 
 const SentimentEnum = {
   NEGATIVE: "NEGATIVE",
@@ -490,30 +231,55 @@ export default function Reviews({ id }: ReviewsProps) {
               <CloseIcon />
             </IconButton>
           </Stack>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item xs={8}>
-              <Typography sx={{ fontWeight: "bold", marginTop: "20px" }}>
-                Good or Bad
-              </Typography>
+          <Box
+            sx={{
+              borderRadius: 5,
+              backgroundColor: "#E5D9F2",
+              elevation: 2,
+              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+              padding: "20px",
+            }}
+          >
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs={10}>
+                <Typography sx={{ fontWeight: "bold" }}>Good or Bad</Typography>
+              </Grid>
+              <Grid item xs={1}>
+                <StyledRating
+                  name="reaction"
+                  value={newReview.reaction === ReactionEnum.GOOD ? 1 : 0}
+                  onChange={(_event, newValue) => {
+                    setNewReview({
+                      ...newReview,
+                      reaction:
+                        newValue === 1 ? ReactionEnum.GOOD : ReactionEnum.BAD,
+                    });
+                  }}
+                  precision={1}
+                  max={1}
+                  icon={<ThumbUpIcon sx={{ color: "#72BF78" }} />}
+                  emptyIcon={<ThumbUpOutlinedIcon />}
+                />
+              </Grid>
+              <Grid item xs={1}>
+                <StyledRating
+                  name="reaction"
+                  value={newReview.reaction === ReactionEnum.BAD ? 1 : 0}
+                  onChange={(_event, newValue) => {
+                    setNewReview({
+                      ...newReview,
+                      reaction:
+                        newValue === 1 ? ReactionEnum.BAD : ReactionEnum.GOOD,
+                    });
+                  }}
+                  precision={1}
+                  max={1}
+                  icon={<ThumbDownIcon sx={{ color: "#FF6500" }} />}
+                  emptyIcon={<ThumbDownOutlined />}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={4}>
-              <StyledRating
-                name="reaction"
-                value={newReview.reaction === ReactionEnum.GOOD ? 1 : 0}
-                onChange={(_event, newValue) => {
-                  setNewReview({
-                    ...newReview,
-                    reaction:
-                      newValue === 1 ? ReactionEnum.GOOD : ReactionEnum.BAD,
-                  });
-                }}
-                precision={1}
-                max={1}
-                icon={<ThumbUpIcon />}
-                emptyIcon={<ThumbUpOutlinedIcon />}
-              />
-            </Grid>
-          </Grid>
+          </Box>
 
           <TextField
             fullWidth
