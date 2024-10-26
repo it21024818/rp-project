d3fee41877955177d4315ce3ba8ff9a13daabee5
Dinuka Dissanayake <it21024818@my.sliit.lk>
2024-10-27 00:34:04 +0530

    security fix

--- web_app/vite.config.ts
+++ web_app/vite.config.ts
@@ -7,4 +7,13 @@ dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net; object-src 'none'",
+      'X-Frame-Options': 'DENY',
+      'X-Content-Type-Options': 'nosniff',
+      'Cross-Origin-Resource-Policy': 'same-origin',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
 })
